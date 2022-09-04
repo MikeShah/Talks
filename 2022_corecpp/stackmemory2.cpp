31faa7d0bb67c2142b02d7456d7f03bf05f0b6d8
Mike <mike@Michaels-MacBook-Air.local>
2022-09-04 04:48:23 +0300

    stack examples

--- 2022_corecpp/stackmemory2.cpp
+++ 2022_corecpp/stackmemory2.cpp
@@ -1,9 +1,11 @@
-// g++ -g -std=c++20 stackmemory.cpp -o prog
+// g++ -g -std=c++20 stackmemory2.cpp -o prog
 #include <iostream>
 int main(){
 
     int x= 42;
+    int y= 42;
     std::cout << &x << '\n';
+    std::cout << &y << '\n';
 
     return 0;
 }
