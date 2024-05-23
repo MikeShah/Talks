1ab7b66305c60793bc3d1c0f800076b986cd76de
Mike <mike@system76-pc.localdomain>
2024-05-23 06:09:29 -0400

    sum_array

--- 2024/cyprus/sum_array.cpp
+++ 2024/cyprus/sum_array.cpp
@@ -1,24 +1,25 @@
 // g++ sum_array.cpp -o prog && time ./prog
-#define ROW 100000
-#define COL 100000
+// sudo perf stat -e cache-misses,branch-misses ./prog
+#define ROW 40000
+#define COL 40000
 
-int sum_array_rows(int* A){
+long sum_array_rows(long* A){
     int sum=0;
 
-    for(int i=0; i < ROW; i++){
-        for(int j=0; j< COL; j++){
+    for(long i=0; i < ROW; i++){
+        for(long j=0; j< COL; j++){
             sum += A[i*COL+j];
         }
     }
     return sum;
 }
 
-int sum_array_cols(int* A){
-    int sum=0;
+long sum_array_cols(long* A){
+    long sum=0;
 
-    for(int j=0; j< COL; j++){
-        for(int i=0; i < ROW; i++){
-           sum += A[i*j];
+    for(long j=0; j< COL; j++){
+        for(long i=0; i < ROW; i++){
+           sum += A[i*COL+j];
         }
     }
     return sum;
@@ -27,10 +28,10 @@ int sum_array_cols(int* A){
 
 int main(){
 
-    int* test= new int[ROW*COL];
+    long* test= new long[ROW*COL];
 
-    //sum_array_cols(test);
-    sum_array_rows(test);
+   //sum_array_cols(test);
+   sum_array_rows(test);
 
     delete[] test;
 
