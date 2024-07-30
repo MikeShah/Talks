7e58b3864c82dfb07efa2a33f915ca9a24920eeb
Mike <mike@system76-pc.localdomain>
2024-07-30 14:59:54 -0400

    removed old example

--- 2024/2024_SIGGRAPH_Blender_Scripting/python_basics/basic4.py
+++
@@ -1,9 +0,0 @@
-
-
-# We can use 'conditional statements' to 'test' a condition 
-# and 'branch' to a different codepath.
-myFriends = ["Mike", "Nick", "Angie"]
-if "Nick" in myFriends:
-	print("Nick is my friend")
-else:
-	print("
