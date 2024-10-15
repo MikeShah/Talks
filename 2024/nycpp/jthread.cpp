9f67b221c11ecc50223ede36a80f0dd6034d7541
Mike <mike@system76-pc.localdomain>
2024-10-15 19:19:42 -0400

    jthread with print and c++23

--- 2024/nycpp/jthread.cpp
+++ 2024/nycpp/jthread.cpp
@@ -3,27 +3,27 @@
 #include <iostream>
 #include <thread>   // Include the thread library
 #include <vector>
+#include <print>
 
 int main() {
+		// This time create a lambda function
+		auto lambda = [](int x){
+				std::println("thread.get_id:{}", std::this_thread::get_id());
+				std::println("Argument passed in:{}", x);
+		};
 
-    // This time create a lambda function
-    auto lambda = [](int x){
-        std::cout << "thread.get_id:" << std::this_thread::get_id() << std::endl;
-        std::cout << "Argument passed in:"   << x << std::endl;
-    };
-    
-    // Note: We now have a jthread
-    //       No joins in the program
-    std::vector<std::jthread> threads;
-  	// Create a collection of threads
-    for(int i=0; i < 10; i++){
-        threads.push_back(std::jthread(lambda,i));
-    } 
+		// Note: We now have a jthread
+		//       No joins in the program
+		std::vector<std::jthread> threads;
+		// Create a collection of threads
+		for(int i=0; i < 10; i++){
+				threads.push_back(std::jthread(lambda,i));
+		} 
 
-	// Continue executing the main thread
-    std::cout << "Hello from the main thread!" << std::endl;
+		// Continue executing the main thread
+		std::cout << "Hello from the main thread!" << std::endl;
 
-    return 0;
+		return 0;
 }
 
 
