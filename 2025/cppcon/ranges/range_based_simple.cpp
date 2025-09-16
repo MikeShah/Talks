016eec94c6da0279c5c890425706dfab7e5f2eff
MikeShah <mshah.475@gmail.com>
2025-09-16 00:24:37 -0400

    examples

---
+++ 2025/cppcon/ranges/range_based_simple.cpp
@@ -0,0 +1,23 @@
+// @file: range_based_simple.cpp
+// Build and run
+// g++-14 -std=c++23 range_based_simple.cpp -o prog && ./prog
+#include <iostream>
+#include <vector>
+#include <print>
+
+int main(int argc, char* argv[]){
+
+  std::vector v = {1,3,5,7};
+  // Iterators defined with a for-loop
+  for(auto& elem : v)
+  {
+
+  }
+
+
+
+
+
+
+  return 0;
+}
---
+++ 2025/cppcon/ranges/structured_binding.cpp
@@ -0,0 +1,35 @@
+// @file: structured_binding.cpp
+// Build and run
+// g++-14 -std=c++23 structured_binding.cpp -o prog && ./prog
+#include <iostream>
+#include <string>
+#include <map>
+#include <print>
+
+int main(int argc, char* argv[]){
+  // An 'ordered' map
+  // Note: I use 'using' in this example so that it's easier
+  //       later on with my '::iterator', or otherwise if I want
+  //       to change the type to something like unordered_map..
+  using StringIntMap = std::map<std::string, int>;
+  StringIntMap myMap {  {"Mike",123},
+                        {"Bjarne", 234},
+                        {"Walter", 345},
+                     };
+
+  // Iterators defined with a for-loop
+  for(auto& [key,value] : myMap)
+
+
+  {
+    std::println("key: {} value: {}", key, value); 
+  }
+  std::println();
+
+
+
+
+
+
+  return 0;
+}
