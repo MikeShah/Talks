64a93648ddc860de3645a085b3619bba8ad19afd
MikeShah <mshah.475@gmail.com>
2026-05-06 13:05:03 -0400

    update

--- 2026/ndctoronto/data.c
+++ 2026/ndctoronto/data.c
@@ -4,33 +4,24 @@
 #include <stdio.h>
 
 /// Zero out the array (note: Could also use 'memset')
-void
-IntArray_Zero (int *array, int length)
-{
+void IntArray_Zero (int *array, int length){
   for (int i = 0; i < length; i++)
     array[i] = 0;
 }
 
 /// Print the array
-void
-IntArray_Print (int *array, int length)
-{
+void IntArray_Print (int *array, int length){
   for (int i = 0; i < length; i++)
     printf ("array[%d]=%d\n", i, array[i]);
 }
 
 /// Increment range of values in an array
-void
-IntArray_Increment (int *array, int start, int end, int increment)
-{
+void IntArray_Increment (int *array, int start, int end, int increment){
   for (int i = start; i < end; i++)
     array[i] += increment;
 }
 
-int
-main ()
-{
-
+int main() {
   int accounts[50];
   IntArray_Zero (accounts, 50);
   IntArray_Increment (accounts, 0, 50, 5);
@@ -38,3 +29,5 @@ main ()
 
   return 0;
 }
+
+
