bb6bce9c3d3e6da3ae7705e5c9649ef78aff8fff
Mike <mshah.475@gmail.com>
2026-09-11 16:06:04 -0400

    hello examples

---
+++ 2026/cppcon/computersystems/hello.cpp
@@ -0,0 +1,6 @@
+// @file: hello.cpp
+#include <print>
+int main(){
+    std::println("Hello cppcon!");
+    return 0;
+}
---
+++ 2026/cppcon/computersystems/hello_small.cpp
@@ -0,0 +1,6 @@
+// @file: hello_small.cpp
+#include <cstdio>
+int main(){
+    std::puts("Hello cppcon!");
+    return 0;
+}
