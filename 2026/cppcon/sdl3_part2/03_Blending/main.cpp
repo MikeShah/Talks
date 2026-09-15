de9adf5ba074d987e65d8dc1d95b4676d89a6ef8
Mike <mshah.475@gmail.com>
2026-09-15 09:49:27 -0400

    implemented

--- 2026/cppcon/sdl3_part2/03_Blending/main.cpp
+++ 2026/cppcon/sdl3_part2/03_Blending/main.cpp
@@ -11,11 +11,6 @@
 #include <SDL3/SDL.h> // For Mac, use <SDL.h>
 
 int main(int argc, char* argv[]){
-  // Create a window data type
-  // This pointer will point to the 
-  // window that is allocated from SDL_CreateWindow
-  SDL_Window* window=nullptr;
-
   // Initialize the video subsystem.
   // iF it returns less than 1, then an
   // error code will be received.
@@ -23,15 +18,18 @@ int main(int argc, char* argv[]){
     std::cout << "SDL could not be initialized: " <<
       SDL_GetError();
   }else{
-    std::cout << "SDL video system is ready to go\n";
+    std::cout << "SDL video & event system is ready to go!\n";
   }
+
+  // Create a window data type
+  // This pointer will point to the 
+  // window that is allocated from SDL_CreateWindow
+  SDL_Window* window=nullptr;
   // Request a window to be created for our platform
   // The parameters are for the title, x and y position,
   // and the width and height of the window.
-  window = SDL_CreateWindow("C++ SDL3 Window", 640,480, 0);
-
-  SDL_Renderer* renderer = nullptr;
-  renderer = SDL_CreateRenderer(window,nullptr);
+  window = SDL_CreateWindow("Mike SDL3 Tutorial - Blending (Press 1-5)", 640,480, 0);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window,nullptr);
 
   SDL_Surface* surface = SDL_LoadBMP("./images/kong.bmp");
   // Set the color key after loading the surface, and before the texture is generated
@@ -43,7 +41,7 @@ int main(int argc, char* argv[]){
   Uint32 colorKey = SDL_MapRGB(details, palette, 0xFF, 0x00, 0xFF);
   // Set the surface
   SDL_SetSurfaceColorKey(surface, true, colorKey);
-
+  // Create the texture
   SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer,surface);
 
   SDL_DestroySurface(surface); 
@@ -61,11 +59,19 @@ int main(int argc, char* argv[]){
   rectangle2.w = 200;
   rectangle2.h = 200;
 
-
   // Infinite loop for our application
   bool gameIsRunning = true;
+  // Hold some state for the purpose of blending in this example
+  int  blendMode = 0;   
   // Main application loop
   while(gameIsRunning){
+    // (3) Clear and Draw the Screen
+    // Gives us a clear "canvas"
+    SDL_SetRenderDrawColor(renderer,0,0,0xFF,SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    // Do our drawing
+    SDL_SetRenderDrawColor(renderer,255,255,255,SDL_ALPHA_OPAQUE);
+
     SDL_Event event;
 
     // (1) Handle Input
@@ -79,36 +85,29 @@ int main(int argc, char* argv[]){
         rectangle2.x = event.motion.x;
         rectangle2.y = event.motion.y;
       }
-      if(event.type == SDL_EVENT_MOUSE_BUTTON_DOWN){
-        if(event.button.button == SDL_BUTTON_LEFT){
-          SDL_SetTextureBlendMode(texture,SDL_BLENDMODE_ADD);
-        }
-        else if(event.button.button == SDL_BUTTON_MIDDLE){
-          SDL_SetTextureBlendMode(texture,SDL_BLENDMODE_BLEND);
-        }
-        else if(event.button.button == SDL_BUTTON_RIGHT){
-          SDL_SetTextureBlendMode(texture,SDL_BLENDMODE_MOD);
-        }
-      }
-      else{
-        SDL_SetTextureBlendMode(texture,SDL_BLENDMODE_BLEND);
-      }
+      
     }
-    // (2) Handle Updates
+    const bool* keystate = SDL_GetKeyboardState(nullptr);
+    if(keystate[SDL_SCANCODE_1]){ blendMode = 1; SDL_Log("1 - BLEND");                }
+    if(keystate[SDL_SCANCODE_2]){ blendMode = 2; SDL_Log("2 - BLEND_PREMULTIPLIED");  }
+    if(keystate[SDL_SCANCODE_3]){ blendMode = 3; SDL_Log("3 - ADD");                  }
+    if(keystate[SDL_SCANCODE_4]){ blendMode = 4; SDL_Log("4 - MOD");                  }
+    if(keystate[SDL_SCANCODE_5]){ blendMode = 5; SDL_Log("5 - MUL");                  }
 
-    // (3) Clear and Draw the Screen
-    // Gives us a clear "canvas"
-    SDL_SetRenderDrawColor(renderer,0,0,0xFF,SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-
-    // Do our drawing
-    SDL_SetRenderDrawColor(renderer,255,255,255,SDL_ALPHA_OPAQUE);
-    //SDL_RenderDrawLine(renderer,5,5,200,220);
-
-    //        SDL_RenderDrawRect(renderer,&rectangle);
+    // Render the first texture normally
+    SDL_SetTextureBlendMode(texture,SDL_BLENDMODE_NONE);
     SDL_RenderTexture(renderer,texture,NULL,&rectangle);
-    SDL_RenderTexture(renderer,texture,NULL,&rectangle2);
 
+    // Render the second texture with blending
+    switch(blendMode){
+      case 1: SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND); break;
+      case 2: SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND_PREMULTIPLIED); break;
+      case 3: SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_ADD); break;
+      case 4: SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_MOD); break;
+      case 5: SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_MUL); break;
+      default: SDL_SetTextureBlendMode(texture,SDL_BLENDMODE_NONE);
+    }
+    SDL_RenderTexture(renderer,texture,NULL,&rectangle2);
 
     // Finally show what we've drawn
     SDL_RenderPresent(renderer);
