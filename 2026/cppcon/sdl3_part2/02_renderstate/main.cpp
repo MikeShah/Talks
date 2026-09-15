dfda38451c256791dc55db63ca1b6f69c42fbaa3
Mike <mshah.475@gmail.com>
2026-09-14 22:14:32 -0400

    02 example

--- 2026/cppcon/sdl3_part2/02_renderstate/main.cpp
+++ 2026/cppcon/sdl3_part2/02_renderstate/main.cpp
@@ -5,38 +5,38 @@
 
 // Program entry point
 int main(){
-	if(!SDL_Init(SDL_INIT_VIDEO)){
-		SDL_Log("Failed!");
-		return -1;
-	}
+    if(!SDL_Init(SDL_INIT_VIDEO)){
+        SDL_Log("Failed!");
+        return -1;
+    }
 
-	SDL_Window* window;
-	window = SDL_CreateWindow("Mike - SDL3", 320, 240, SDL_WINDOW_RESIZABLE);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
-	
-	// Clear any custom render state and use the default
-	SDL_SetGPURenderState(renderer, nullptr);
+    SDL_Window* window;
+    window = SDL_CreateWindow("Mike - SDL3", 320, 240, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
 
-typedef struct SDL_GPURenderStateCreateInfo
-{
-    SDL_GPUShader *fragment_shader; /**< The fragment shader to use when this render state is active */
+    // Clear any custom render state and use the default
+    SDL_SetGPURenderState(renderer, nullptr);
 
-    Sint32 num_sampler_bindings;    /**< The number of additional fragment samplers to bind when this render state is active */
-    const SDL_GPUTextureSamplerBinding *sampler_bindings;   /**< Additional fragment samplers to bind when this render state is active */
+    typedef struct SDL_GPURenderStateCreateInfo
+    {
+        SDL_GPUShader *fragment_shader; /**< The fragment shader to use when this render state is active */
 
-    Sint32 num_storage_textures;    /**< The number of storage textures to bind when this render state is active */
-    SDL_GPUTexture *const *storage_textures;    /**< Storage textures to bind when this render state is active */
+        Sint32 num_sampler_bindings;    /**< The number of additional fragment samplers to bind when this render state is active */
+        const SDL_GPUTextureSamplerBinding *sampler_bindings;   /**< Additional fragment samplers to bind when this render state is active */
 
-    Sint32 num_storage_buffers;     /**< The number of storage buffers to bind when this render state is active */
-    SDL_GPUBuffer *const *storage_buffers;      /**< Storage buffers to bind when this render state is active */
+        Sint32 num_storage_textures;    /**< The number of storage textures to bind when this render state is active */
+        SDL_GPUTexture *const *storage_textures;    /**< Storage textures to bind when this render state is active */
 
-    SDL_PropertiesID props;         /**< A properties ID for extensions. Should be 0 if no extensions are needed. */
-} SDL_GPURenderStateCreateInfo;
+        Sint32 num_storage_buffers;     /**< The number of storage buffers to bind when this render state is active */
+        SDL_GPUBuffer *const *storage_buffers;      /**< Storage buffers to bind when this render state is active */
+
+        SDL_PropertiesID props;         /**< A properties ID for extensions. Should be 0 if no extensions are needed. */
+    } SDL_GPURenderStateCreateInfo;
 
 
-	SDL_Delay(5000);
+    SDL_Delay(5000);
 
-	SDL_Quit();
+    SDL_Quit();
 
-	return 0;
+    return 0;
 }
